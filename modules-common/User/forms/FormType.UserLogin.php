132c638633fb5c6df0b9116124bbf812a4fd7542
Istvan Ujj-Meszaros <istvan@radaptor.com>
2026-03-31 23:06:17 +0200

    Refine login form navigation flow

--- modules-common/User/forms/FormType.UserLogin.php
+++ modules-common/User/forms/FormType.UserLogin.php
@@ -22,7 +22,7 @@ class FormTypeUserLogin extends AbstractForm
 	public static function getDefaultPathForCreation(): array
 	{
 		return [
-			'path' => '/admin/',
+			'path' => '/',
 			'resource_name' => 'login.html',
 			'layout' => 'admin_empty',
 		];
@@ -49,6 +49,8 @@ class FormTypeUserLogin extends AbstractForm
 
 		if (Request::_GET('loginreferer', false) !== false) {
 			Url::redirect(Url::sanitizeRefererUrl((string) Request::_GET('loginreferer')));
+		} elseif ($this->isCanonicalLoginResourceRequest()) {
+			Url::redirect(Url::getCurrentHost());
 		} else {
 			Url::redirect(Url::getCurrentUrl());
 		}
@@ -89,5 +91,29 @@ class FormTypeUserLogin extends AbstractForm
 		if (is_null(User::getUserByUsernameAndPassword($this->getInput('username'), $this->getInput('password')))) {
 			$this->getInput('username')->addError(t('user.login.error_invalid_credentials'));
 		}
+	}
+
+	private function isCanonicalLoginResourceRequest(): bool
+	{
+		$page_id = ResourceTypeWebpage::getWebpageIdByFormType(FormList::USERLOGIN);
+
+		if (!is_int($page_id) || $page_id <= 0) {
+			return false;
+		}
+
+		$login_url = Url::getSeoUrl($page_id);
+
+		if (!is_string($login_url) || trim($login_url) === '') {
+			return false;
+		}
+
+		$current_path = parse_url(Url::getCurrentUrl(), PHP_URL_PATH);
+		$login_path = parse_url($login_url, PHP_URL_PATH);
+
+		if (!is_string($current_path) || !is_string($login_path) || $current_path === '' || $login_path === '') {
+			return false;
+		}
+
+		return rtrim($current_path, '/') === rtrim($login_path, '/');
 	}
 }

691de79aff37107c28b2193925fcacb9306acc09
Istvan Ujj-Meszaros <istvan@radaptor.com>
2026-05-13 14:13:44 +0200

    Fix RichText validation locale

--- modules-common/RichText/forms/customValidators/FormCustomValidator.RichText.php
+++ modules-common/RichText/forms/customValidators/FormCustomValidator.RichText.php
@@ -18,9 +18,7 @@ abstract class FormCustomValidatorRichText extends AbstractForm
 			return;
 		}
 
-		$locale = $locale_input !== null && trim((string) $locale_input->getValue()) !== ''
-			? LocaleService::canonicalize((string) $locale_input->getValue())
-			: RichTextLocaleService::getLocaleForCurrentRequest();
+		$locale = $this->resolveValidationLocale($locale_input);
 		$id = EntityRichtext::getContentIdByName($this->getInput('name')->getValue(), $locale);
 
 		if ($id == EntityRichtext::ERROR_NOT_FOUND) {
@@ -30,5 +28,30 @@ abstract class FormCustomValidatorRichText extends AbstractForm
 		if ($id !== $this->getItemId()) {
 			$this->getInput('name')->addError(t('cms.richtext.field.name.unique_error'));
 		}
+	}
+
+	private function resolveValidationLocale(?FormInput $locale_input): string
+	{
+		if ($locale_input !== null && trim((string) $locale_input->getValue()) !== '') {
+			return LocaleService::canonicalize((string) $locale_input->getValue());
+		}
+
+		$current_locale = LocaleService::tryCanonicalize((string) ($this->initvalues['locale'] ?? ''));
+
+		if ($current_locale !== null) {
+			return $current_locale;
+		}
+
+		$item_id = $this->getItemId();
+
+		if ($item_id > 0) {
+			$saved_locale = EntityRichtext::getContentLocale($item_id);
+
+			if ($saved_locale !== null) {
+				return $saved_locale;
+			}
+		}
+
+		return RichTextLocaleService::getLocaleForCurrentRequest();
 	}
 }
